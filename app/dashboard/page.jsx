8673286fb2f6162b1be1c2c0b1f86328d1d81ca5
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2024-08-20 19:46:52 +0200

    update

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='h-screen bg-red-600'>
-        <h1>Dashboard</h1>
-        <Link href="/">go back HOME</Link>
+    <div className='h-screen bg-red-600 grid items-center justify-center'>
+        <div className="text-4xl">
+          <h1>Dashboard</h1>
+          <Link href="/">go back HOME</Link>
+        </div>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -3,9 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>project todo</h1>
-      <Link href="/dashboard">dashboard</Link>
+    <main className="grid items-center justify-center h-screen">
+      <div className="text-2xl">
+        <h1>project todo</h1>
+        <Link href="/dashboard">dashboard</Link>
+      </div>
     </main>
   );
 }
